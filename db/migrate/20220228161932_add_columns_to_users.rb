5e43d5a1c7c4213dbe4eddc8d884c503e14d1dca
Caroline Dana <caroline.m.dana@gmail.com>
2022-02-28 17:27:33 +0100

    added all routes

---
+++ db/migrate/20220228161932_add_columns_to_users.rb
@@ -0,0 +1,8 @@
+class AddColumnsToUsers < ActiveRecord::Migration[6.1]
+  def change
+    add_column :users, :first_name, :string
+    add_column :users, :last_name, :string
+    add_column :users, :job_wanted, :string
+    add_column :users, :skills, :string
+  end
+end
