dd8a4bc1cdca543e755701087023d45fce60c238
simon desplan <simon.desplan@gmail.com>
2022-03-02 12:35:12 +0100

    css sign up et front + controller user

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,22 @@
 class UsersController < ApplicationController
+  def show
+    @user = User.find(params[:id])
+  end
+
+  def edit
+    @user = User.find(params[:id])
+  end
+
+  def update
+    @user = User.find(params[:id])
+    @user.update(params[:user])
+    # Will raise ActiveModel::ForbiddenAttributesError
+    redirect_to user_path(@user)
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:first_name, :last_name, :job_wanted, :soft_skills, :hard_skills, :languages, :years_of_experience, :values)
+  end
 end
