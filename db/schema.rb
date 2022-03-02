70f16f920577901e61b3a77a2809562aed6272d9
Aurore Gosset <auroregosset.pro@gmail.com>
2022-03-02 17:42:20 +0100

    end seed

--- db/schema.rb
+++ db/schema.rb
@@ -107,9 +107,9 @@ ActiveRecord::Schema.define(version: 2022_03_02_151809) do
     t.string "name"
     t.string "category"
     t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
+    t.datetime "updated_at", zprecision: 6, null: false
   end
-  
+
   create_table "meetings", force: :cascade do |t|
     t.string "name"
     t.datetime "start_time"
