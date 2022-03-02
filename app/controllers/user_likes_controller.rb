75ee3bc5ff3288a6d89880a2bb9884b3edd01977
Caroline Dana <caroline.m.dana@gmail.com>
2022-03-02 11:42:23 +0100

    user likes create fini et job index back fini

--- app/controllers/user_likes_controller.rb
+++ app/controllers/user_likes_controller.rb
@@ -1,11 +1,22 @@
 class UserLikesController < ApplicationController
   def create
-    @user_like = UserLike.new(job_offer: JobOffer.find(params[:job_offer_id]))
+    @job_offer = JobOffer.find(params[:job_offer_id])
+    @user_like = UserLike.new(job_offer: @job_offer)
     @user_like.user = current_user
-    @user_like.status = 'En attente'
+
+    if params[:dislike]
+      @user_like.status = false
+    else
+      @user_like.status = true
+    end
 
     if @user_like.save
-      redirect_to job_offers_path
+      if @user_like.status = true && JobLike.find_by(user: current_user, job_offer: @job_offer, status: true)
+        match = Match.create(user: current_user, job_offer: @job_offer)
+        redirect_to match_path(match)
+      else
+        redirect_to job_offers_path
+      end
     else
       redirect_to job_offers_path
     end
