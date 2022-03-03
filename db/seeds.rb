3e5acf4c3a5111241be5fe81a048ba5a9403ba0c
Aurore Gosset <auroregosset.pro@gmail.com>
2022-03-03 16:48:51 +0100

    Mise à jour seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -17,7 +17,7 @@ def get_image(image)
 end
 
 puts 'creating user 1'
-user_1 = User.create!(first_name: "Caroline", last_name: "Dana", email: "caroline.m.dana@gmail.com", password: "mdpmdp", job_wanted: "Product Owner", languages: "Français, Anglais", years_of_experience: 8)
+user_1 = User.create!(first_name: "Caroline", last_name: "Dana", email: "caroline.m.dana@gmail.com", password: "mdpmdp", job_wanted: "Product Owner", languages: "Français Anglais", years_of_experience: 8)
 puts 'created Caroline'
 
 puts 'seeding companies'
@@ -97,8 +97,21 @@ value_12 = Skill.create(name: "Liberté d'expression", category: "values")
 value_13 = Skill.create(name: "Responsabilisation", category: "values")
 value_14 = Skill.create(name: "Indépendance", category: "values")
 value_15 = Skill.create(name: "Respect", category: "values")
-
 puts 'seeding skills done'
+
+puts 'seeding user skills'
+UserSkill.create(user: user_1, skill: soft_skill_1)
+UserSkill.create(user: user_1, skill: soft_skill_2)
+UserSkill.create(user: user_1, skill: soft_skill_3)
+
+UserSkill.create(user: user_1, skill: hard_skill_7)
+UserSkill.create(user: user_1, skill: hard_skill_9)
+UserSkill.create(user: user_1, skill: hard_skill_13)
+
+UserSkill.create(user: user_1, skill: value_1)
+UserSkill.create(user: user_1, skill: value_2)
+UserSkill.create(user: user_1, skill: value_3)
+puts 'seeding user skills done'
 
 puts 'seeding job offers'
 
@@ -108,17 +121,12 @@ job_offer_1 = JobOffer.new(
   company: company_1,
   city: "Paris",
   contracttype: "CDI",
-  description: "En tant que chef de produit pour l'automatisation du marketing, vous serez chargé de construire les meilleurs outils du secteur pour soutenir nos efforts d'acquisition. Intégré dans une équipe, travaillant avec des ingénieurs seniors et des data scientists, vous devrez construire l'avenir de nos outils de marketing. Vous serez responsable de l'ensemble du projet, de la définition de la stratégie à la feuille de route et à l'expédition des fonctionnalités. Vos missions :
-  Recueillir des insights sur les problématiques Marketing et les traduire en opportunités actionnables qui permettraient d'améliorer leurs performances.
-  Définir la stratégie et l'ambition associée.
-  Construire la feuille de route associée pour la réaliser.
+  description: "Vos missions : Recueillir des insights sur les problématiques Marketing et les traduire en opportunités.
+  Définir la stratégie et l'ambition associée. Construire la feuille de route des fonctionnalités.
   Collaborer avec l'ingénierie au sein d'une équipe pour exécuter la feuille de route.
   Communiquer sur le plan de l'équipe et les réalisations au sein de BlaBlaCar.",
-  # hard_skills: "Software development, Bases User stories, Methodologie Agile, SCRUM",
-  # soft_skills: "Créativité, Esprit critique, Curieux, Communication, Esprit d'équipe",
   languages: "Français Anglais",
   years_of_experience: "5",
-  # values: "Succès, Engagement, Empathie, Apprentissage, Humilité",
   question1: "Quelle méthode privilégiez vous ? Scrum, Kanban, Scrumban ? Autres ?",
   question2: "Comment collectez-vous les retours utilisateurs ?",
   question3: "Comment validez-vous une nouvelle UX ou interface ?"
@@ -134,16 +142,11 @@ job_offer_2 = JobOffer.new(
   company: company_2,
   city: "Paris",
   contracttype: "CDI",
-  description: "En tant que premier Product Growth Manager de l'équipe, vous jouerez un rôle clé dans l'évolution de notre stratégie de croissance et aurez l'opportunité d'apporter de la valeur à Skello dès le premier jour ! Vos missions :
-  Analyser nos performances : vous identifiez les principaux et pain points sur nos chemins de conversion avec des données qualitatives & quantitatives et mesurez l'impact de nouvelles actions.
-  Définir la feuille de route de conversion du site web/des landings: vous identifiez & priorisez les projets, fonctionnalités & idées pertinentes pour augmenter notre conversion pour nos 5 marchés (France, Espagne, Allemagne, Suisse & Belgique).
-  Améliorer les KPI de conversion : vous développez une méthode de test A/B pour tester de nouvelles idées, obtenir des insights et améliorer continuellement les taux de conversion du site web (landing pages), mais aussi sur notre produit (post sign-up).
-  S'assurer que nos pages de destination sont à jour et localisées pour chaque marché en fonction du positionnement de notre marque et de nos dernières fonctionnalités/partenariats : vous travaillez en étroite collaboration avec le marketing, le marketing produit et les équipes locales pour vous assurer que nos nouveaux produits ou fonctionnalités sont visibles, bien compris et qu'ils incitent les prospects à convertir.",
-  # hard_skills: "Logiciel SEO, CRM, Data analysis, Software development, Bases User stories",
-  # soft_skills: "Esprit d'équipe, Organisation, Créativité, Patient, Curiosité",
-  languages: "Français, Anglais, Allemand",
+  description: "Vos missions : Analyser nos performances, identifiez les principaux pain points. Définir la feuille de route de conversion du site web.
+  Identifiez & priorisez les projets, fonctionnalités & idées pertinentes. Améliorer les KPI de conversion, méthode de test A/B. Obtenir des insights sur notre produit.
+  S'assurer que nos pages de destination soient à jour.",
+  languages: "Français Anglais",
   years_of_experience: "5",
-  # values: "Passion, Ownership, Humilité, Empathie, Tenacité",
   question1: "Quelle méthode privilégiez vous ? Scrum, Kanban, Scrumban ? Autres ?",
   question2: "Comment collectez-vous les retours utilisateurs ?",
   question3: "Comment validez-vous une nouvelle UX ou interface ?"
@@ -159,23 +162,11 @@ job_offer_3 = JobOffer.new(
   company: company_3,
   city: "Paris",
   contracttype: "CDI",
-  description: "Au sein de l'équipe produit, vous travaillerez sur un périmètre dédié de fonctionnalités et avec de nombreuses équipes de l'entreprise (data, juridique, marketing, tech, opérations...) pour améliorer la valeur apportée aux utilisateurs par vos fonctionnalités. Les missions :
-  Comprendre les utilisateurs via des analyses de marché, des entretiens avec les utilisateurs, des analyses comportementales et une collaboration avec les équipes du service client et des ventes.
-  Comprendre le paysage concurrentiel de vos fonctionnalités
-  Surveiller les performances et la convivialité des fonctionnalités à l'aide d'analyses.
-  Définir les évolutions et les améliorations du produit, en collaboration avec un concepteur de produits et un chef de produit technique.
-  Rédiger des User Stories et les présenter à l'équipe.
-  Suivez la mise en œuvre technique des évolutions de votre produit.
-  Tester vos fonctionnalités et itérer
-  Créer et maintenir la documentation sur vos fonctionnalités
-  Communiquer en interne sur vos fonctionnalités, leurs évolutions et la vision du produit qu'elles portent.
-  S'assurer que vos fonctionnalités sont correctement communiquées aux utilisateurs, en collaboration avec l'équipe CRM et Marketing.
-  Participer à la définition de la feuille de route",
-  # hard_skills: "CRM, Analytics, Relation client, Software development, Bases UX",
-  # soft_skills: "Rigueur, Problem solving, Esprit d'équipe, Organisation, Enthousiasme",
-  languages: "Français, Anglais",
+  description: "Vos missions : Surveiller les performances à l'aide d'analyses. Définir les évolutions et les améliorations du produit.
+  Rédiger des User Stories. Tester les nouvelles fonctionnalités. Créer la documentation sur les fonctionnalités.
+  Communiquer en interne sur les fonctionnalités et leurs évolutions. Participer à la définition de la feuille de route.",
+  languages: "Français Anglais",
   years_of_experience: "8",
-  # values: "Loyauté, Bienveillance, Ownership, Humilité, Tenacité",
   question1: "Quelle méthode privilégiez vous ? Scrum, Kanban, Scrumban ? Autres ?",
   question2: "Comment collectez-vous les retours utilisateurs ?",
   question3: "Comment validez-vous une nouvelle UX ou interface ?"
@@ -187,8 +178,19 @@ job_offer_3.save!
 
 puts 'seeding job offers done'
 
+puts 'seeding job skills'
+[job_offer_1, job_offer_2].each do |job_offer|
+  [soft_skill_4, soft_skill_5, soft_skill_6, hard_skill_1, hard_skill_2, hard_skill_3, value_4, value_5, value_6,].each do |skill|
+    JobSkill.create(job_offer: job_offer, skill: skill)
+  end
+end
+
+[soft_skill_1, soft_skill_2, soft_skill_3, hard_skill_7, hard_skill_9, hard_skill_13, value_1, value_2, value_3].each do |skill|
+  JobSkill.create(job_offer: job_offer_3, skill: skill)
+end
+puts 'seeding job skills done'
+
 puts 'seeding job likes'
 # Lydia
 job_like_3 = JobLike.create(user: user_1, job_offer: job_offer_3, status: true)
-
 puts 'seeding job likes done'
