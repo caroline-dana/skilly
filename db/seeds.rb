6f3b0871b6d1e337a4d75ccc5e14e39854229675
simon desplan <simon.desplan@gmail.com>
2022-03-03 15:44:31 +0100

    fixe seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,6 +5,7 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+JobSkill.destroy_all
 JobLike.destroy_all
 JobOffer.destroy_all
 UserLike.destroy_all
